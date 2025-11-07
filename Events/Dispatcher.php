23b644ab2d7342beb87b7dd1d91ac607f87c386b
Alexander Campo <jalexcam@gmail.com>
2025-11-07 16:39:59 -0500

    Added new method for fire an event in the interface

--- Events/Dispatcher.php
+++ Events/Dispatcher.php
@@ -16,7 +16,7 @@
  * @package     Lenevor
  * @subpackage  Base
  * @link        https://lenevor.com
- * @copyright   Copyright (c) 2019 - 2024 Alexander Campo <jalexcam@gmail.com>
+ * @copyright   Copyright (c) 2019 - 2025 Alexander Campo <jalexcam@gmail.com>
  * @license     https://opensource.org/licenses/BSD-3-Clause New BSD license or see https://lenevor.com/license or see /license.md
  */
 
@@ -94,6 +94,17 @@ interface Dispatcher
      * @return array|null
      */
     public function until($event, $payload = []);
+
+    /**
+     * Fire an event and call the listeners.
+     *
+     * @param  string|object  $event
+     * @param  mixed  $payload
+     * @param  bool  $halt
+     * 
+     * @return array|null
+     */
+    public function fire($event, $payload = [], $halt = false);
 
     /**
      * Dispatch an event and call the listeners.
