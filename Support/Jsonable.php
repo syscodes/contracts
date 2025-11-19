307b7d0f994e20313a98dece404066d056451aab
Alexander Campo <jalexcam@gmail.com>
2025-11-18 19:30:18 -0500

    Added the type output of method toJson() in Jsonable interface

--- Support/Jsonable.php
+++ Support/Jsonable.php
@@ -16,7 +16,7 @@
  * @package     Lenevor
  * @subpackage  Base
  * @link        https://lenevor.com
- * @copyright   Copyright (c) 2019 - 2024 Alexander Campo <jalexcam@gmail.com>
+ * @copyright   Copyright (c) 2019 - 2025 Alexander Campo <jalexcam@gmail.com>
  * @license     https://opensource.org/licenses/BSD-3-Clause New BSD license or see https://lenevor.com/license or see /license.md
  */
 
@@ -34,5 +34,5 @@ interface Jsonable
      * 
      * @return string
      */
-    public function toJson($options = 0);
+    public function toJson($options = 0): string;
 }
